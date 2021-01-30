dd450dc48f89bf6f7585175ccf9723357e332a34
Md. Nahidul Islam <nahidulss@gmail.com>
2021-01-30 17:50:30 +0600

    git learnig :local to remote

--- main.go
+++ main.go
@@ -4,5 +4,11 @@ func main() {
 
 	fmt.Print("Hello World")
 	fmt.Println("Hello Master Academy")
-	fmt.Println("Git commit testing")
+	fmt.Println("Git commit testing") 
+	fmt.Print("Todays Outcome:git  stages,add,push,commit ")
+	fmt.Println("First text modifyed then add to stages area")
+	fmt.Println("commit:stages to local repo ") 
+	fmt.Println("push:local to remote repo ") 
+	
+	
 }
