38c5f909bb90004f68a1a93086b282a947f38270
Md. Nahidul Islam <nahidulss@gmail.com>
2021-01-31 20:44:49 +0600

    initial variable code

---
+++ variable_8.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+	var nickname string = "nahid"
+	var firstName,lastName string = "MD. Nahidul" ,"Islam"
+fmt.Println(firstName,lastName, nickname)
+var age int=23
+var c rune='A'
+var marks float32 =50.45
+fmt.Println(age,c,marks)	
+}
+
