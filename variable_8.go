3e1cb9ead3c29372f970206f3b47498b0ff4bddb
Md. Nahidul Islam <nahidulss@gmail.com>
2021-01-31 22:37:51 +0600

    modified 1

--- variable_8.go
+++ variable_8.go
@@ -3,10 +3,10 @@ import "fmt"
 func main(){
 	var nickname string = "nahid"
 	var firstName,lastName string = "MD. Nahidul" ,"Islam"
-fmt.Println(firstName,lastName, nickname)
+fmt.Println("Name:",firstName,lastName, "nickname=", nickname)
 var age int=23
 var c rune='A'
 var marks float32 =50.45
-fmt.Println(age,c,marks)	
+fmt.Println("age=",age,"ascii value of char=",c,"marks=",marks)	
 }
 
