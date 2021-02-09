1936ba23e52c1936f21e3f214c6ac8a2179a1218
Md. Nahidul Islam <nahidulss@gmail.com>
2021-02-09 17:20:19 +0600

    initial commit

---
+++ class 5/main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main() {
+
+	fmt.Print("Hello World")
+	fmt.Println("Hello Master Academy")
+	fmt.Println("Git commit testing") 
+	fmt.Print("Todays Outcome:git  stages,add,push,commit ")
+	fmt.Println("First text modifyed then add to stages area")
+	fmt.Println("commit:stages to local repo ") 
+	fmt.Println("push:local to remote repo ") 
+	
+}
