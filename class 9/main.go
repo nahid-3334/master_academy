d47dafc94564b9033a2b915876bbb6a60dadeecd
Md. Nahidul Islam <nahidulss@gmail.com>
2021-02-10 23:13:34 +0600

    boolean ,rune

---
+++ class 9/main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main()  {
+	//boolean data type 
+// 	fmt.Println(true && true)
+//   fmt.Println(true && false)
+//   fmt.Println(true || true)
+//   fmt.Println(true || false)
+//   fmt.Println(!true)	
+//rune -- verbs
+var c,c1 rune ='A','অ'
+fmt.Printf("%c=%d\n",c,c)
+fmt.Printf("%c=%d",c1,c1)
+}
