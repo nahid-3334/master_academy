d7e7c2af949fa9357fd949997c01a75175a9bcc2
Md. Nahidul Islam <nahidulss@gmail.com>
2021-02-12 23:28:15 +0600

    added copy

--- class 10/main.go
+++ class 10/main.go
@@ -34,4 +34,9 @@ func main()  {
 	 //type by reflect
 	 a:=reflect.TypeOf(fruits).Kind().String()
 	 fmt.Println(a)
+	 //Here is an example of copy:
+	slice1 := []int{1,2,3}
+	slice2 := make([]int, 2)
+	copy(slice2, slice1)
+	fmt.Println(slice1, slice2)
 }
