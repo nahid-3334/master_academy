7c3b7a48ff6973d75ba7fdff7c33debde9eee1fe
Md. Nahidul Islam <nahidulss@gmail.com>
2021-02-13 00:22:05 +0600

    maps of maps

--- class 10/main.go
+++ class 10/main.go
@@ -13,23 +13,78 @@ fmt.Println(x)
 //delete 
 delete(x, "key")
 fmt.Println(x)
-elements := make(map[string]string)
-elements["H"] = "Hydrogen"
-elements["He"] = "Helium"
-elements["Li"] = "Lithium"
-elements["Be"] = "Beryllium"
-elements["B"] = "Boron"
-elements["C"] = "Carbon"
-elements["N"] = "Nitrogen"
-elements["O"] = "Oxygen"
-elements["F"] = "Fluorine"
-elements["Ne"] = "Neon"
+// elements := make(map[string]string)
+// elements["H"] = "Hydrogen"
+// elements["He"] = "Helium"
+// elements["Li"] = "Lithium"
+// elements["Be"] = "Beryllium"
+// elements["B"] = "Boron"
+// elements["C"] = "Carbon"
+// elements["N"] = "Nitrogen"
+// elements["O"] = "Oxygen"
+// elements["F"] = "Fluorine"
+// elements["Ne"] = "Neon"
 
-fmt.Println(elements["Li"])
-fmt.Println(elements)
-fmt.Println(elements["p"])
-if name, ok := elements["Un"]; ok {
-	fmt.Println(name, ok)
+// fmt.Println(elements["Li"])
+// fmt.Println(elements)
+// fmt.Println(elements["p"])
+// students[0] = "Apu"
+// 	students[1]="Robin"
+// 	students[2]="John"
+// 	students[3]="Robiul"
+// 	students[4]="Foysal"
+// 	//get
+// 	fmt.Println(students[3])
+// 	fmt.Println(students[4])
+// 	//short hand implicit=...
+// 	marks:=[...]int{80,40,70,87,76}
+// 	fmt.Println(marks)
+
+
+//maps of maps
+elements := map[string]map[string]string{
+    "H": map[string]string{
+      "name":"Hydrogen",
+      "state":"gas",
+    },
+    "He": map[string]string{
+      "name":"Helium",
+      "state":"gas",
+    },
+    "Li": map[string]string{
+      "name":"Lithium",
+      "state":"solid",
+    },
+    "Be": map[string]string{
+      "name":"Beryllium",
+      "state":"solid",
+    },
+    "B":  map[string]string{
+      "name":"Boron",
+      "state":"solid",
+    },
+    "C":  map[string]string{
+      "name":"Carbon",
+      "state":"solid",
+    },
+    "N":  map[string]string{
+      "name":"Nitrogen",
+      "state":"gas",
+    },
+    "O":  map[string]string{
+      "name":"Oxygen",
+      "state":"gas",
+    },
+    "F":  map[string]string{
+      "name":"Fluorine",
+      "state":"gas",
+    },
+    "Ne":  map[string]string{
+      "name":"Neon",
+      "state":"gas",
+    },
   }
-
+  if el, ok := elements["UN"]; ok {
+    fmt.Println(el["name"], el["state"])
+  }
 }
