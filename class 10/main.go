501f1e32b3a27c6593ed0797605046f108c5360c
Md. Nahidul Islam <nahidulss@gmail.com>
2021-02-13 00:03:06 +0600

    maps

--- class 10/main.go
+++ class 10/main.go
@@ -1,42 +1,35 @@
 package main
-
 import "fmt"
-import "reflect"
-func main()  {
-	//Array
-	var students [5]string
-	fmt.Println(students)
-	fmt.Println(len(students))
-	//len for length empty string for default
-	//set 
-	students[0] = "Apu"
-	students[1]="Robin"
-	students[2]="John"
-	students[3]="Robiul"
-	students[4]="Foysal"
-	//get
-	fmt.Println(students[3])
-	fmt.Println(students[4])
-	//short hand implicit=...
-	marks:=[...]int{80,40,70,87,76}
-	fmt.Println(marks)
-	//slice =sub array 
-	//short way & from array  hold starting index to last value-1 
-	twostudents:=students[1:2]
-	fmt.Println(twostudents)
-	//direct way by makes
-	x:=make([]string,5,10)
-	fmt.Println(x)
-	 var fruits []string
-	 fruits=append(fruits,"apple","mango","orange","banana")
-	 //Type
-	 fmt.Printf("%T %s\n",fruits,fruits)
-	 //type by reflect
-	 a:=reflect.TypeOf(fruits).Kind().String()
-	 fmt.Println(a)
-	 //Here is an example of copy:
-	slice1 := []int{1,2,3}
-	slice2 := make([]int, 2)
-	copy(slice2, slice1)
-	fmt.Println(slice1, slice2)
+func main() {
+
+// x["key"] = 10
+// fmt.Println(x)this get panic error without using make
+x := make(map[string]int)
+x["key"] = 10
+x["room"]=100
+x["id"]=54
+fmt.Println(x["key"])
+fmt.Println(x)
+//delete 
+delete(x, "key")
+fmt.Println(x)
+elements := make(map[string]string)
+elements["H"] = "Hydrogen"
+elements["He"] = "Helium"
+elements["Li"] = "Lithium"
+elements["Be"] = "Beryllium"
+elements["B"] = "Boron"
+elements["C"] = "Carbon"
+elements["N"] = "Nitrogen"
+elements["O"] = "Oxygen"
+elements["F"] = "Fluorine"
+elements["Ne"] = "Neon"
+
+fmt.Println(elements["Li"])
+fmt.Println(elements)
+fmt.Println(elements["p"])
+if name, ok := elements["Un"]; ok {
+	fmt.Println(name, ok)
+  }
+
 }
