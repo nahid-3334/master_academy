35a4298012e31c2d173934f403084d633ea727ae
Md. Nahidul Islam <nahidulss@gmail.com>
2021-02-14 12:01:46 +0600

    flow control for

--- class 5/main.go
+++ class 5/main.go
@@ -1,6 +1,7 @@
-package main
+/*package main
 import "fmt"
 func main() {
+
 
 	fmt.Print("Hello World")
 	fmt.Println("Hello Master Academy")
@@ -10,4 +11,29 @@ func main() {
 	fmt.Println("commit:stages to local repo ") 
 	fmt.Println("push:local to remote repo ") 
 	
+}
+*/
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	str1 := "1234"
+
+	/** converting the str1 variable into an int using Atoi method */
+	i1, err := strconv.Atoi(str1)
+	if err == nil {
+		fmt.Println(i1)
+	}
+
+	str2 := "5678"
+	/** converting the str2 variable into an int using ParseInt method */
+	i2, err := strconv.ParseInt(str2, 10, 64)
+	if err == nil {
+		fmt.Println(i2)
+	}
 }
--- class_12/flow_control.go
+++ class_12/flow_control.go
@@ -17,25 +17,41 @@ func main() {
 	// }
 	//marks group using switch
 	//fixed value
-	var marks int
-	fmt.Println("Enter yor marks:")
-	fmt.Scanf("%d", &marks)
+	// var marks int
+	// fmt.Println("Enter yor marks:")
+	// fmt.Scanf("%d", &marks)
 // >=80	A+
 // 70-79	A
 // 60-69	B
 // 50-59   C
 // 40-49	D
-	switch marks/10 {
-	case 4:
-		fmt.Println("your grade D")
-	case 5:
-		fmt.Println("your grade C")
-	case 6:
-		fmt.Println("your grade B")
-	case 7:
-		fmt.Println("your grade A")
-	case 8,9,10:
-		fmt.Println("your grade A+")
+	// switch marks/10 {
+	// case 4:
+	// 	fmt.Println("your grade D")
+	// case 5:
+	// 	fmt.Println("your grade C")
+	// case 6:
+	// 	fmt.Println("your grade B")
+	// case 7:
+	// 	fmt.Println("your grade A")
+	// case 8,9,10:
+	// 	fmt.Println("your grade A+")
 
+	// }
+	//loop 
+	//mormal
+	for i := 0; i<10; i++ {
+		fmt.Println(i,"nahid")
+	}
+	//get value from array range loop
+ 	students :=[]string{"apu","foysal","Robiul"}
+	for i,std:=range students{
+		fmt.Println(i,std)
+	}
+	//while in for
+	i:=0
+	for i<10{
+		fmt.Println(i,"nahid")
+		i++
 	}
 }
