93885377cd971e107040f78f38ae849faea47668
Md. Nahidul Islam <nahidulss@gmail.com>
2021-02-14 11:47:19 +0600

    flow control if & switch

---
+++ class_12/flow_control.go
@@ -0,0 +1,41 @@
+package main
+import "fmt"
+func main() {
+	//flow control
+	//age grouping if else
+	// var age int
+	// fmt.Println("Enter yor age:")
+	// fmt.Scanf("%d", &age)
+	// if age < 3{
+	// 	fmt.Println("infant")
+	// }else if age >=3 && age <=12{
+	// 	fmt.Println("Child")
+	// }else if age>=13 && age <=18{
+	// 	fmt.Println("Teenage")
+	// }else{
+	// 	fmt.Println("adult")
+	// }
+	//marks group using switch
+	//fixed value
+	var marks int
+	fmt.Println("Enter yor marks:")
+	fmt.Scanf("%d", &marks)
+// >=80	A+
+// 70-79	A
+// 60-69	B
+// 50-59   C
+// 40-49	D
+	switch marks/10 {
+	case 4:
+		fmt.Println("your grade D")
+	case 5:
+		fmt.Println("your grade C")
+	case 6:
+		fmt.Println("your grade B")
+	case 7:
+		fmt.Println("your grade A")
+	case 8,9,10:
+		fmt.Println("your grade A+")
+
+	}
+}
