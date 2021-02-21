50ecd2e80da5dc4a5b4fcca0fce8de99feaade2c
Md. Nahidul Islam <nahidulss@gmail.com>
2021-02-21 19:45:10 +0600

    pointer

---
+++ class_14/pointer.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func update(s *int){
+	fmt.Println("s=",*s)
+	*s= *s +10
+}
+func main() {
+	var x int
+	var y *int
+	fmt.Println("zero value x: ", x,"\nzero value y",y)
+	fmt.Println("address x: ",&x," address y:",&y)
+	x=10//assign
+	y= &x //referencing
+	fmt.Println("value x: ", x,"\nvalue y",y)//accessing
+	fmt.Println("y dererencing value is", *y)//dereferencing
+	*y=20
+	fmt.Println("value x: ", x,"\ndereference value y:",*y)
+	update(&x)
+	fmt.Println("value x=",x)
+}
